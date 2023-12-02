1f1edb8e6bd4a60ed88474e13e2d6ed38916f70e
gaspar burgi <gasparburgi49@gmail.com>
2023-12-02 13:43:20 -0300

    3 Done

--- taxifare/interface/workflow.py
+++ taxifare/interface/workflow.py
@@ -11,20 +11,38 @@ from taxifare.params import *
 
 @task
 def preprocess_new_data(min_date: str, max_date: str):
-    pass  # YOUR CODE HERE
+    return preprocess(min_date,max_date)
 
 @task
 def evaluate_production_model(min_date: str, max_date: str):
-    pass  # YOUR CODE HERE
+    return evaluate(min_date,max_date)
 
 @task
-def re_train(min_date: str, max_date: str, split_ratio: str):
-    pass  # YOUR CODE HERE
+def re_train(min_date: str, max_date: str, split_ratio: float):
+    return train(min_date=min_date,max_date=max_date,split_ratio=split_ratio)
 
 @task
 def transition_model(current_stage: str, new_stage: str):
-    pass  # YOUR CODE HERE
+    mlflow_transition_model(current_stage=current_stage,new_stage=new_stage)
 
+@task
+def notify(old_mae, new_mae):
+    """
+    Notify about the performance
+    """
+    base_url = 'https://wagon-chat.herokuapp.com'
+    channel = '1357' # Change to your batch number
+    url = f"{base_url}/{channel}/messages"
+    author = 'GasparinB' # Change this to your github nickname
+    if new_mae < old_mae and new_mae < 2.5:
+        content = f"🚀 New model replacing old in production with MAE: {new_mae} the Old MAE was: {old_mae}"
+    elif old_mae < 2.5:
+        content = f"✅ Old model still good enough: Old MAE: {old_mae} - New MAE: {new_mae}"
+    else:
+        content = f"🚨 No model good enough: Old MAE: {old_mae} - New MAE: {new_mae}"
+    data = dict(author=author, content=content)
+    response = requests.post(url, data=data)
+    response.raise_for_status()
 
 @flow(name=PREFECT_FLOW_NAME)
 def train_flow():
@@ -40,7 +58,14 @@ def train_flow():
     min_date = EVALUATION_START_DATE
     max_date = str(datetime.strptime(min_date, "%Y-%m-%d") + relativedelta(months=1)).split()[0]
 
-    pass  # YOUR CODE HERE
+    preprocessed=preprocess_new_data.submit(min_date=min_date,max_date=max_date)
+    old_mae = evaluate_production_model.submit(min_date=min_date,max_date=max_date,wait_for=[preprocessed])
+    new_mae = re_train.submit(min_date=min_date,max_date=max_date,split_ratio=0.2,wait_for=[preprocessed])
+    notify.submit(old_mae=old_mae.result(),new_mae=new_mae.result(),wait_for=[old_mae,new_mae])
+    print(f"old_mae: {old_mae} -- new_mae: {new_mae}")
+    if 2.5 > new_mae.result() > old_mae.result():
+      transition_model.submit(current_stage='Staging',new_stage='Production',wait_for=[old_mae,new_mae])
+
 
 if __name__ == "__main__":
     train_flow()
